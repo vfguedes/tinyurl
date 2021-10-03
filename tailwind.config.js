1ca3e3d10c01ab0ef17e26ac183e519756266f30
--help <victorfelipe.guedes@hotmail.com>
2021-10-02 21:13:56 -0300

    :wrench: Add Tailwind configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  mode: 'jit',
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
