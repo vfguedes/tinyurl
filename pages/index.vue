9ca7f6956c0104fc5311b7a67c869f4448511ec2
--help <victorfelipe.guedes@hotmail.com>
2021-10-02 21:12:37 -0300

    :fire: Remove default files

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <Tutorial />
+  <div class="h-screen flex items-center justify-center">
+    <h1 class="text-3xl font-bold text-green-400">tinyurl</h1>
+  </div>
 </template>
 
 <script lang="ts">
